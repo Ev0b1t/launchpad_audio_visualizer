e0b524e58f5a81d5b2e0ec6d68f5acd9e77a0f91
ev0b1t <fake@mail.ru>
2025-08-30 20:00:46 +0500

    feat: 🎸 add lights up at top and bottom side

--- core/laucnhpad_visualization.py
+++ core/laucnhpad_visualization.py
@@ -1,10 +1,30 @@
 import asyncio
 from launchpad_py import launchpad
 from core.config import CONFIG
-from core.state import BANDS_POS
+from core.state import BANDS_POS, VISUAL_STATE_CACHE
+
+
+class VisualizerState:
+    def __init__(self):
+        self.smoothed_sub = 0.0
+        self.smoothed_bass = 0.0
+        self.smoothed_high = 0.0
+        self.smoothed_mid_high = 0.0
+        self.smoothed_low_mid_high = 0.0
+
+
+VSTATE = VisualizerState()
+
+# Smoothing coefficients
+ALPHA_SUB = 0.8
+ALPHA_BASS = 0.8
+ALPHA_HIGH = 0.3
+ALPHA_MID_HIGH = 0.4
+ALPHA_LOW_MID_HIGH = 0.3
 
 
 async def visualize_audio_bands(lp: launchpad.LaunchpadPro, bands_arr):
+    global VSTATE
     tasks = []
 
     for pad_x, val in enumerate(bands_arr):
@@ -36,7 +56,9 @@ async def visualize_audio_bands(lp: launchpad.LaunchpadPro, bands_arr):
     if tasks:
         await asyncio.gather(*tasks)
 
-    await _visualize_side_buttons(lp, bands_arr[0], bands_arr[1])
+    await _visualize_side_buttons(
+        lp, bands_arr[0], bands_arr[1],
+        bands_arr[-4], bands_arr[-3], bands_arr[-2])
 
 
 async def _visualize_pad_button(
@@ -57,46 +79,91 @@ async def _visualize_pad_button(
     lp.LedCtrlXY(pad_x, pad_y, r, g, b)
 
 
-async def _visualize_side_buttons(lp: launchpad.LaunchpadPro, sub_level: float, bass_level: float):
+async def _visualize_side_buttons(
+        lp: launchpad.LaunchpadPro, sub_level: float,
+        bass_level: float, low_mid_high: float, mid_high_level: float, high_level: float):
     """
     Side button visualization with smooth color transitions based on bass level.
     """
-    left_x = 9
-    right_x = 8
+    global VSTATE, ALPHA_SUB, ALPHA_BASS, ALPHA_HIGH, ALPHA_MID_HIGH, VISUAL_STATE_CACHE
+
+    # 1. UPDATE THE SMOOTHED VALUES using the EMA
+    VSTATE.smoothed_sub = ALPHA_SUB * sub_level + (1 - ALPHA_SUB) * VSTATE.smoothed_sub
+    VSTATE.smoothed_bass = ALPHA_BASS * bass_level + (1 - ALPHA_BASS) * VSTATE.smoothed_bass
+    VSTATE.smoothed_high = ALPHA_HIGH * high_level + (1 - ALPHA_HIGH) * VSTATE.smoothed_high
+    VSTATE.smoothed_mid_high = ALPHA_MID_HIGH * mid_high_level + (1 - ALPHA_MID_HIGH) * VSTATE.smoothed_mid_high
+    VSTATE.smoothed_low_mid_high = ALPHA_MID_HIGH * low_mid_high + (1 - ALPHA_LOW_MID_HIGH) * VSTATE.smoothed_low_mid_high
+
+    lx = 9
+    rx = 8
+    ty = 0
+    by = 9
+
     lr = lg = lb = 0
     rr = rg = rb = 0
+    tr = tg = tb = 0
+    br = bg = bb = 0
 
     intensity = sub_level ** 1.1
 
-    # TODO: different modes automatically
-    # for ex. together or unique
-
     if sub_level > 0.3:
-        # Interpolate color from a start color to an end color
         start_color = (0, 0, 63)
         end_color = (63, 0, 0)
-
-        # Scale sub level to a 0-1 range for a color gradient.
-        # Use a power function to make it more sensitive to strong bass hits.
         lr = int(start_color[0] + (end_color[0] - start_color[0]) * intensity)
         lg = int(start_color[1] + (end_color[1] - start_color[1]) * intensity)
         lb = int(start_color[2] + (end_color[2] - start_color[2]) * intensity)
 
     if bass_level > 0.5:
-        # Interpolate color from a start color to an end color
         start_color = (63, 0, 0)
         end_color = (0, 0, 63)
-
-        # Scale bass_level to a 0-1 range for a color gradient.
-        # Use a power function to make it more sensitive to strong bass hits.
         rr = int(start_color[0] + (end_color[0] - start_color[0]) * intensity)
         rg = int(start_color[1] + (end_color[1] - start_color[1]) * intensity)
         rb = int(start_color[2] + (end_color[2] - start_color[2]) * intensity)
 
-    # left
-    for y in range(1, 9):
-        lp.LedCtrlXY(left_x, y, lr, lg, lb)
+    # Control the upper (green) and lower (orange) buttons
+    # Top buttons (green) - for bass
+    if VSTATE.smoothed_bass > 0.9 and VSTATE.smoothed_sub > 0.9 and sub_level > 0.9 and bass_level > 0.9:
+        tr, tg, tb = 0, 63, 0
+    else:
+        tr, tg, tb = 0, int(VSTATE.smoothed_bass * 63), 0
 
-    # right
+    # Bottom buttons (orange) - for snares
+    if VSTATE.smoothed_high > 0.7 and VSTATE.smoothed_mid_high > 0.7:
+        br, bg, bb = 63, 20, 0
+    else:
+        smoothed_combined = (VSTATE.smoothed_high + VSTATE.smoothed_mid_high) / 2
+        br, bg, bb = int(smoothed_combined * 63), int(smoothed_combined * 20), 0
+
+    #2. CACHING AND UPDATING LEDS
+
+    # Left side buttons
     for y in range(1, 9):
-        lp.LedCtrlXY(right_x, CONFIG.pads.PADS_IN_COLUMN - y + 1, rr, rg, rb)
+        current_color = (lr, lg, lb)
+        cache_index = y - 1 # 0 - 7
+        if VISUAL_STATE_CACHE[cache_index] != current_color:
+            lp.LedCtrlXY(lx, y, lr, lg, lb)
+            VISUAL_STATE_CACHE[cache_index] = current_color
+
+    # Right side buttons
+    for y in range(1, 9):
+        current_color = (rr, rg, rb)
+        cache_index = 8 + (y - 1) # 8 - 15
+        if VISUAL_STATE_CACHE[cache_index] != current_color:
+            lp.LedCtrlXY(rx, CONFIG.pads.PADS_IN_COLUMN - y + 1, rr, rg, rb)
+            VISUAL_STATE_CACHE[cache_index] = current_color
+
+    # Top buttons
+    for x in range(0, 8):
+        current_color = (tr, tg, tb)
+        cache_index = 16 + x # 16 - 23
+        if VISUAL_STATE_CACHE[cache_index] != current_color:
+            lp.LedCtrlXY(x, ty, tr, tg, tb)
+            VISUAL_STATE_CACHE[cache_index] = current_color
+
+    # Bottom buttons
+    for x in range(0, 8):
+        current_color = (br, bg, bb)
+        cache_index = 24 + x # 24 - 31
+        if VISUAL_STATE_CACHE[cache_index] != current_color:
+            lp.LedCtrlXY(x, by, br, bg, bb)
+            VISUAL_STATE_CACHE[cache_index] = current_color
--- core/state.py
+++ core/state.py
@@ -1,4 +1,7 @@
 # Global variables which will be using at the runtime moment
 
+# state of side buttons
+VISUAL_STATE_CACHE = [(0, 0, 0) for _ in range(32)]
+
 BANDS_POS = []
 EMA_GLOBAL_PEAK = 1.0
